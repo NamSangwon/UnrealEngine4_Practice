b68e3f96cd831c9ba1be3c9e25e9552b4cdd2c51
NamSangwon <nsw3265@naver.com>
2024-02-01 16:10:37 +0900

    init UE4 Component & Collision Practice

---
+++ Source/UE4_Collision/UE4_CollisionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_CollisionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_COLLISION_API AUE4_CollisionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
