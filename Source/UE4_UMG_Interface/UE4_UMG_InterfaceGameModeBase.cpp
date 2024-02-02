ad62f90a5e00e0dbc2f6ef1c5cf81ba427ee1673
NamSangwon <nsw3265@naver.com>
2024-02-02 15:18:22 +0900

    add setting for using UMG (change widget func)

--- Source/UE4_UMG_Interface/UE4_UMG_InterfaceGameModeBase.cpp
+++ Source/UE4_UMG_Interface/UE4_UMG_InterfaceGameModeBase.cpp
@@ -2,4 +2,26 @@
 
 
 #include "UE4_UMG_InterfaceGameModeBase.h"
+#include "Blueprint/UserWidget.h"
 
+void AUE4_UMG_InterfaceGameModeBase::BeginPlay(){
+    Super::BeginPlay();
+
+    ChangeMenuWidget(StartingWidgetClass); // 게임 시작 시 StartingWidgetClass 위젯을 표시
+}
+
+void AUE4_UMG_InterfaceGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass){
+    // 현재 출력하고 있는 widget 비우기
+    if (CurrentWidget != nullptr){
+        CurrentWidget->RemoveFromViewport();
+        CurrentWidget = nullptr;
+    }
+
+    // 입력 받은 위젯으로 변경
+    if (NewWidgetClass != nullptr){
+        CurrentWidget = CreateWidget(GetWorld(), NewWidgetClass);
+        if (CurrentWidget != nullptr){
+            CurrentWidget->AddToViewport(); // 위젯 출력
+        }
+    }
+}
--- Source/UE4_UMG_Interface/UE4_UMG_InterfaceGameModeBase.h
+++ Source/UE4_UMG_Interface/UE4_UMG_InterfaceGameModeBase.h
@@ -13,5 +13,19 @@ UCLASS()
 class UE4_UMG_INTERFACE_API AUE4_UMG_InterfaceGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	// 위젯 변경 함수
+	UFUNCTION(BlueprintCallable, Category="UMG_Game")
+	void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 게임 시작 시 표시될 UI 위젯을 저장
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG_Game")
+	TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	// 현재 화면에 표시되고 있는 UI 위젯을 저장
+	UPROPERTY()
+	UUserWidget* CurrentWidget;
 };
