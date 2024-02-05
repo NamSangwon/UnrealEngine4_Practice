3044685ad3dff001b9fd321e30d7933589999ac7
NamSangwon <nsw3265@naver.com>
2024-02-05 19:35:57 +0900

    init UE4 FPS Practice

---
+++ Source/UE4_FPS/UE4_FPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_FPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_FPS_API AUE4_FPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
