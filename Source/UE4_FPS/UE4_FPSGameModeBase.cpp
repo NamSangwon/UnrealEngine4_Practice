f37a5d64ed590144d3bae3dafb00ca82a2dafdb2
NamSangwon <nsw3265@naver.com>
2024-02-05 20:02:39 +0900

    fix setting for applying modified Game Mode

--- Source/UE4_FPS/UE4_FPSGameModeBase.cpp
+++ Source/UE4_FPS/UE4_FPSGameModeBase.cpp
@@ -3,3 +3,11 @@
 
 #include "UE4_FPSGameModeBase.h"
 
+void AUE4_FPSGameModeBase::StartPlay(){
+    Super::StartPlay();
+
+    if(GEngine){
+        // 뷰포트 화면에 로그 띄우기
+        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, This is FPS Game Mode!")); 
+    }
+}
--- Source/UE4_FPS/UE4_FPSGameModeBase.h
+++ Source/UE4_FPS/UE4_FPSGameModeBase.h
@@ -14,4 +14,9 @@ class UE4_FPS_API AUE4_FPSGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	// 게임이 시작될 떄, 게임 모드에서 액터들의 BeginPlay()를 호출하는 함수
+	virtual void StartPlay() override; 
+
+
 };
